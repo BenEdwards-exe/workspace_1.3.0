ae9ec42807950367271ac31f02ade3a1728e0b4a
BenEdwards-exe <61501540+BenEdwards-exe@users.noreply.github.com>
2020-10-29 20:07:29 +0200

    hos tokkelos

--- Space Invaders/Core/Src/stm32f4xx_it.c
+++ Space Invaders/Core/Src/stm32f4xx_it.c
@@ -44,6 +44,7 @@
 /* USER CODE BEGIN PV */
 extern uint8_t refresh;
 extern uint8_t player_fire;
+extern uint8_t continue_input;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -226,6 +227,20 @@ void EXTI4_IRQHandler(void)
   /* USER CODE BEGIN EXTI4_IRQn 1 */
 
   /* USER CODE END EXTI4_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line[9:5] interrupts.
+  */
+void EXTI9_5_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI9_5_IRQn 0 */
+	continue_input = 1;
+  /* USER CODE END EXTI9_5_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_8);
+  /* USER CODE BEGIN EXTI9_5_IRQn 1 */
+
+  /* USER CODE END EXTI9_5_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
